249b3a60074872aacf9a3e829b30dd9b6c94b780
jhpiedrahitao <jhpiedrahitao@unal.edu.co>
2021-05-10 12:25:06 -0500

    virtual mouse activation / deactivation feature added

--- VirtualAIMouse.py
+++ VirtualAIMouse.py
@@ -5,6 +5,8 @@ import HandTrackingModule as htm
 import time
 import autopy
 from autopy.mouse import Button
+from playsound import playsound
+import os
 
 wCam=1280
 hCam=720
@@ -12,12 +14,17 @@ wScr,hScr=autopy.screen.size()
 frameReduction=100
 smoothening=8
 
+activatedAudioPath = os.path.join("audios","mouseActive.mp3")
+deactivatedAudioPath = os.path.join("audios","mouseNotActive.mp3")
+
 cap=cv2.VideoCapture(1)
 cap.set(3,wCam)
 cap.set(4,hCam)
 
 pTime=0
 mode="n"
+vMouseActive=False
+vMouseActivated=False
 lastMode="n"
 pLocX,pLocY=0,0
 cLocX,cLocY=0,0
@@ -30,7 +37,16 @@ while True:
     img=detector.findHands(img)
     lmList=detector.findPosition(img,draw=False)
     if len(lmList)!=0:
-        bbox=detector.boundingBox(img,draw=False)
+        bbox=detector.boundingBox(img,draw=True)
+        if (bbox[2]-bbox[0])*(bbox[3]-bbox[1]) > (wCam*hCam/3): 
+            if vMouseActivated==False:
+                vMouseActivated=True
+                vMouseActive = not vMouseActive
+                if vMouseActive: playsound(activatedAudioPath)
+                else: playsound(deactivatedAudioPath)
+        else: vMouseActivated=False
+        
+
         x1,y1=lmList[5][1:]
         xS=np.interp(x1,(frameReduction,wCam-frameReduction),(0,wScr-1),0,wScr-1)
         yS=np.interp(y1,(frameReduction,hCam-frameReduction),(0,hScr-1),0,hScr-1)
@@ -38,16 +54,19 @@ while True:
         cLocY=pLocY+(yS-pLocY)/smoothening
         
         fingersUP=detector.fingersUP()
-        if fingersUP[1:]==[1,1,1,1]: mode="m"
-        elif fingersUP[:]==[1,0,0,0,0]: mode="l"
-        elif fingersUP[1:]==[0,1,1,1]: mode="t"
-        elif fingersUP[1:]==[1,1,0,0]: mode="r"
-        elif fingersUP[:]==[0,0,0,0,0]: mode="s"
+        if vMouseActive:
+            if fingersUP[1:]==[1,1,1,1]: mode="m"
+            elif fingersUP[:]==[1,0,0,0,0]: mode="l"
+            elif fingersUP[1:]==[0,1,1,1]: mode="t"
+            elif fingersUP[1:]==[1,1,0,0]: mode="r"
+            elif fingersUP[:]==[0,0,0,0,0]: mode="s"
+            else: mode="n"
         else: mode="n"
     else: mode="n"
 
     if mode!="n":
         if mode!="l":
+            #autopy.mouse.smooth_move(cLocX,cLocY)
             autopy.mouse.move(cLocX,cLocY)
         pLocX,pLocY = cLocX,cLocY
         if mode=='t': 
